0ad9622eec357dd59135336c84290ddeb4a0d20e
AryaneLadeira <aryanemottals@gmail.com>
2021-08-25 21:32:18 -0300

    Fazendo o binding entre os elementos da tela e o controller

--- app/src/main/java/com/example/appmedidas/MainActivity.java
+++ app/src/main/java/com/example/appmedidas/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.appmedidas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputName, inputAge, inputHeight, inputWeight;
+    Button calculateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputName = findViewById(R.id.edTxtName);
+        inputAge = findViewById(R.id.edTxtNumberAge);
+        inputHeight = findViewById(R.id.edTxtNumberHeight);
+        inputWeight = findViewById(R.id.edTxtNumberWeight);
+        calculateBtn = findViewById(R.id.buttonCalculate);
+
     }
 }
