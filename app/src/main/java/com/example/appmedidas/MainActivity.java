24c1ea3873e0775e75ccf943d0423c4c281b5dc6
AryaneLadeira <aryanemottals@gmail.com>
2021-08-25 22:32:18 -0300

    Criando o onclick no controller

--- app/src/main/java/com/example/appmedidas/MainActivity.java
+++ app/src/main/java/com/example/appmedidas/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.appmedidas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.appmedidas.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
         inputWeight = findViewById(R.id.edTxtNumberWeight);
         calculateBtn = findViewById(R.id.buttonCalculate);
 
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = inputName.getText().toString();
+                double weight = Double.valueOf(inputWeight.getText().toString());
+                double height = Double.valueOf(inputHeight.getText().toString());
+                int age = Integer.valueOf(inputAge.getText().toString());
+
+                Pessoa pessoa = new Pessoa(name, weight , height, age);
+                Toast.makeText(getApplicationContext(), "Olá "+name+" , "+age+" anos, "+weight+" kg e "+height+" de altura."+"\n Seu IMC é: "
+                        +pessoa.imc()+" - "+pessoa.resultado(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
